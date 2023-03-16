c3b344f709f92315532c75d0472620af2fa6626e
Pankz <pankznegi4638@gmail.com>
2023-03-16 19:24:46 +0530

    getting user object i.e M of MVC

--- src/main/java/com/pankz/service/LoginService.java
+++ src/main/java/com/pankz/service/LoginService.java
@@ -1,7 +1,21 @@
 package com.pankz.service;
 
+import java.util.HashMap;
+
+import com.pankz.dto.User;
+
 public class LoginService {
-	//dummy method
+	HashMap<String,String> users=new HashMap<String,String>();
+	public LoginService() //dummy DB
+	{
+		users.put("pankz", "Pankaj");
+		users.put("ankt","Ankit" );
+		users.put("jguru","javaguru");
+				
+			
+		
+	}
+	//dummy method for authentication---->(Business Service)
 	public boolean authenticate(String userId,String password)
 	{
 		if(password==null || password.trim()=="")
@@ -10,5 +24,11 @@ public class LoginService {
 		}
 		return true;
 	}
-
+public User getUserDetails(String userId)
+{
+	User user=new User();
+	user.setUserName(users.get(userId));
+	user.setUserId(userId);
+	 return user ;
+}
 }
